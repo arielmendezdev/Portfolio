c1d48439ee5be774d34b7a694a74ffe6a4aeef9c
ariosmen <cristian@waaiit.site>
2025-01-01 12:55:38 -0300

    Agregado de proyectos

--- src/app/proyectos/page.jsx
+++ src/app/proyectos/page.jsx
@@ -1,5 +1,37 @@
+import { pages } from "@/complementos/pages";
+import { Button } from "@mui/material";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Proyectos() {
   return (
-    <div>Proyectos</div>
-  )
+    <div className="flex justify-center flex-col mx-10 gap-11 mt-16">
+      {pages.map((proyect, index) => {
+        return (
+          <Card key={index} className="flex">
+            <CardContent>
+              <Image src={proyect.img} alt="image"/>
+            </CardContent>
+            <CardContent>
+              <Typography variant="h4">{proyect.name}</Typography>
+              <Typography component="div">{proyect.text}</Typography>
+              <Typography
+                sx={{ color: "text.secondary", mb: 1.5 }}
+              ></Typography>
+              <Typography variant="body2"></Typography>
+              <CardActions>
+                <Link href={proyect.to}>
+                  <Button>Ver página</Button>
+                </Link>
+              </CardActions>
+            </CardContent>
+          </Card>
+        );
+      })}
+    </div>
+  );
 }
